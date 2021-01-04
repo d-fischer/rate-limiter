6269211ad59002356e6faddfcf4fa3ace02e4f76
Daniel Fischer <daniel@d-fischer.dev>
2021-01-04 14:11:19 +0100

    fix passthru config

--- src/TimedPassthruRateLimiter.ts
+++ src/TimedPassthruRateLimiter.ts
@@ -3,7 +3,7 @@ import type { TimeBasedRateLimiterConfig } from './TimeBasedRateLimiter';
 import { TimeBasedRateLimiter } from './TimeBasedRateLimiter';
 
 export class TimedPassthruRateLimiter<Req, Res> extends TimeBasedRateLimiter<Req, Res> {
-	constructor(private readonly _child: RateLimiter<Req, Res>, config: TimeBasedRateLimiterConfig) {
+	constructor(private readonly _child: RateLimiter<Req, Res>, config: TimeBasedRateLimiterConfig<Req, Res>) {
 		super(config);
 	}
 
