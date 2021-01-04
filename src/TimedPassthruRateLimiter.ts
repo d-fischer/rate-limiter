8d7390f0bce10bc0af09d9dd50fa043303a045b5
Daniel Fischer <daniel@d-fischer.dev>
2021-01-04 14:14:12 +0100

    properly do passthru with new options layout

--- src/TimedPassthruRateLimiter.ts
+++ src/TimedPassthruRateLimiter.ts
@@ -3,11 +3,12 @@ import type { TimeBasedRateLimiterConfig } from './TimeBasedRateLimiter';
 import { TimeBasedRateLimiter } from './TimeBasedRateLimiter';
 
 export class TimedPassthruRateLimiter<Req, Res> extends TimeBasedRateLimiter<Req, Res> {
-	constructor(private readonly _child: RateLimiter<Req, Res>, config: TimeBasedRateLimiterConfig<Req, Res>) {
-		super(config);
-	}
-
-	protected async doRequest(req: Req): Promise<Res> {
-		return this._child.request(req);
+	constructor(child: RateLimiter<Req, Res>, config: Omit<TimeBasedRateLimiterConfig<Req, Res>, 'doRequest'>) {
+		super({
+			...config,
+			async doRequest(req: Req): Promise<Res> {
+				return child.request(req);
+			}
+		});
 	}
 }
